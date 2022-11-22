44b1fea455215ccfdc620354e1ab59431941f3a3
baharalidurrani <baharalidurrani@gmail.com>
2022-11-23 00:02:17 +0500

    GraphQL Auto Generated Schema File Added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,61 +1,82 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
-import { GetUserArgs } from "./dto/args/get-user.args";
-import { GetUsersArgs } from "./dto/args/get-users.args";
-import { CreateUserInput } from "./dto/input/create-user.input";
-import { DeleteUserInput } from "./dto/input/delete-user.input";
-import { UpdateUserInput } from "./dto/input/update-user.input";
-import { User } from "./models/user";
+import { GetUserArgs } from './dto/args/get-user.args';
+import { GetUsersArgs } from './dto/args/get-users.args';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { User } from './models/user';
 
 @Injectable()
-export class UsersService {
-    private users: User[] = [
-        {
-            email: 'dan@example.com',
-            password: 'mypassword',
-            userId: '123',
-            age: 20,
-        }
-    ];
+export class UsersService implements OnModuleInit {
+  static data: string;
 
-    public createUser(createUserData: CreateUserInput): User {
-        const user: User = {
-            userId: uuidv4(),
-            ...createUserData
-        }
+  onModuleInit(): void {
+    UsersService.data = 'init';
+  }
 
-        this.users.push(user);
+  public getTest(): string {
+    return UsersService.data;
+  }
+  public postTest(data: string): void {
+    UsersService.data = data;
+  }
 
-        return user;
-    }
+  private users: User[] = [
+    {
+      email: 'dan@example.com',
+      password: 'mypassword',
+      userId: '123',
+      age: 20,
+    },
+    {
+      email: 'a@g.c',
+      password: 'pass',
+      userId: '124',
+      age: 23,
+    },
+  ];
 
-    public updateUser(updateUserData: UpdateUserInput): User {
-        const user = this.users.find(user => user.userId === updateUserData.userId);
+  public createUser(createUserData: CreateUserInput): User {
+    const user: User = {
+      userId: uuidv4(),
+      ...createUserData,
+    };
 
-        Object.assign(user, updateUserData);
+    this.users.push(user);
 
-        return user;
-    }
+    return user;
+  }
 
-    public getUser(getUserArgs: GetUserArgs): User {
-        return this.users.find(user => user.userId === getUserArgs.userId);
-    }
+  public updateUser(updateUserData: UpdateUserInput): User {
+    const user = this.users.find(user => user.userId === updateUserData.userId);
 
-    public getUserByEmail(email: string): User | undefined {
-        return this.users.find(user => user.email === email);
-    }
+    Object.assign(user, updateUserData);
 
-    public getUsers(getUsersArgs: GetUsersArgs): User[] {
-        return getUsersArgs.userIds.map(userId => this.getUser({ userId }));
-    }
+    return user;
+  }
 
-    public deleteUser(deleteUserData: DeleteUserInput): User {
-        const userIndex = this.users.findIndex(user => user.userId === deleteUserData.userId);
+  public getUser(getUserArgs: GetUserArgs): User {
+    return this.users.find(user => user.userId === getUserArgs.userId);
+  }
 
-        const user = this.users[userIndex];
+  public getUserByEmail(email: string): User | undefined {
+    return this.users.find(user => user.email === email);
+  }
 
-        this.users.splice(userIndex);
+  public getUsers(getUsersArgs: GetUsersArgs): User[] {
+    return getUsersArgs.userIds.map(userId => this.getUser({ userId }));
+  }
 
-        return user;
-    }
+  public deleteUser(deleteUserData: DeleteUserInput): User {
+    const userIndex = this.users.findIndex(
+      user => user.userId === deleteUserData.userId,
+    );
+
+    const user = this.users[userIndex];
+
+    this.users.splice(userIndex);
+
+    return user;
+  }
 }
